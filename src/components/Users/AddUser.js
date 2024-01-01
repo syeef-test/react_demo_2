9c2a438a2698c8841028106e22e24f455b15a0b4
syeef <syeefislam@yahoo.com>
2024-01-01 08:15:49 +0530

    custom wrapper and built in wrapper

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import classes from "../UI/Card.module.css";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
@@ -44,7 +45,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -72,7 +73,7 @@ const AddUser = (props) => {
           <button type="submit">Add</button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
